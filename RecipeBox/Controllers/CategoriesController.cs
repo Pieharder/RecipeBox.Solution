6ea6662b698162c965d0b4c24b0f2a1958608244
Drake Wilcox <drakewilcox@gmail.com>
2020-03-24 11:32:18 -0700

    add routes and methods for create, details, and edit.
    Co-authored-by: Geoff Goetz <geoff.goetz503@gmail.com>
    Co-authored-by: Jamison Cozart <jamisoncozart@gmail.com>

--- RecipeBox/Controllers/CategoriesController.cs
+++ RecipeBox/Controllers/CategoriesController.cs
@@ -1,8 +1,9 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using RecipeBox.Models;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.EntityFrameworkCore;
 
 namespace RecipeBox.Controllers
 {
@@ -13,6 +14,57 @@ namespace RecipeBox.Controllers
     public CategoriesController(RecipeBoxContext db)
     {
       _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      return View(_db.Categories.ToList());
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Category category, int RecipeId)
+    {
+      _db.Categories.Add(category);
+      if(RecipeId != 0)
+      {
+        _db.CategoryRecipe.Add(new CategoryRecipe() { RecipeId = RecipeId, CategoryId = category.CategoryId});
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      Category thisCategory = _db.Categories
+        .Include(category => category.Recipes)
+        .ThenInclude(join => join.Recipe)
+        .FirstOrDefault(category => category.CategoryId == id);
+      return View(thisCategory);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      Category thisCategory = _db.Categories.FirstOrDefault(category => category.CategoryId == id);
+      ViewBag.CategoryId = new SelectList(_db.Recipes, "RecipeId", "Name");
+      return View(thisCategory);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Category category, int RecipeId)
+    {
+      if(RecipeId != 0)
+      {
+        _db.CategoryRecipe.Add(new CategoryRecipe() { RecipeId = RecipeId, CategoryId = category.CategoryId });
+      }
+      _db.Entry(category).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
     }
   }
 }
