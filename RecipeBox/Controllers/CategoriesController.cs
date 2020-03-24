e0dc2e2c5219b077405c35da2e9cba8b4e8d1e86
Drake Wilcox <drakewilcox@gmail.com>
2020-03-24 11:56:49 -0700

    add routes for addRecipe and delete.
    Co-authored-by: Jamison Cozart <jamisoncozart@gmail.com>
    Co-authored-by: Geoff Goetz <geoff.goetz503@gmail.com>

--- RecipeBox/Controllers/CategoriesController.cs
+++ RecipeBox/Controllers/CategoriesController.cs
@@ -66,5 +66,57 @@ namespace RecipeBox.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+
+    public ActionResult AddRecipe(int id)
+    {
+      Category thisCategory = _db.Categories.FirstOrDefault(categories => categories.CategoryId == id);
+      ViewBag.RecipeId = new SelectList(_db.Recipes, "RecipeId", "Name");
+      return View(thisCategory);
+    }
+
+    [HttpPost]
+    public ActionResult AddRecipe(Category category, int RecipeId)
+    {
+      if(RecipeId != 0)
+      {
+        _db.CategoryRecipe.Add(new CategoryRecipe() { RecipeId = RecipeId, CategoryId = category.CategoryId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    // public ActionResult Delete(int id)
+    // {
+    //   Category thisCategory = _db.Categories.FirstOrDefault(patient => patient.CategoryId == id);
+    //   return View(thisCategory);
+    // }
+    public ActionResult Delete(int id)
+    {
+      var thisCategory = _db.Categories.FirstOrDefault(category => category.CategoryId == id);
+      return View(thisCategory);
+      // var joinEntry = _db.CategoryRecipe.FirstOrDefault(entry => entry.CategoryRecipeId == id);
+      // _db.CategoryRecipe.Remove(joinEntry);
+      // _db.SaveChanges();
+      // return RedirectToAction("Index");
+    }
+    
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisCategory = _db.Categories.FirstOrDefault(category => category.CategoryId == id);
+      _db.Categories.Remove(thisCategory);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+      // Category thisCategory = _db.Categories.FirstOrDefault(patients => patients.CategoryId == id);
+      // List<CategoryRecipe> categoryRecipes = _db.CategoryRecipe.Where(recipeCategory => recipeCategory.CategoryId == id).ToList();
+      // foreach(CategoryRecipe dp in categoryRecipes)
+      // {
+      //   _db.CategoryRecipe.Remove(dp);
+      // }
+      // _db.Categories.Remove(thisCategory);
+      // _db.SaveChanges();
+      // return RedirectToAction("Index");
+    }
+
   }
 }
