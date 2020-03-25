34047d6428bb8ff91c4c67f26e152cc0ba0d020f
Drake Wilcox <drakewilcox@gmail.com>
2020-03-25 14:44:26 -0700

    add color selector and update migrations and add styling.
    Co-authored-by: Jamison Cozart <jamisoncozart@gmail.com>
    Co-authored-by: Geoff Goetz <geoff.goetz503@gmail.com>

---
+++ RecipeBox/Migrations/20200325212114_Color.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RecipeBox.Migrations
+{
+    public partial class Color : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Color",
+                table: "Recipes",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Color",
+                table: "Recipes");
+        }
+    }
+}
--- RecipeBox/Models/Recipe.cs
+++ RecipeBox/Models/Recipe.cs
@@ -11,6 +11,7 @@ namespace RecipeBox.Models
     public int RecipeId { get; set; }
 		public string Name { get; set; }
 		public string Description { get; set; }
+		public string Color { get; set; }
 		public virtual ApplicationUser User { get; set; }
 		public virtual ICollection<CategoryRecipe> Categories { get; }
 	}
