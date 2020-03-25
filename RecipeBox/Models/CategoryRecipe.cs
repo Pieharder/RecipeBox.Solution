69a80df9a8399356c05ffd0d013432455190fe76
Drake Wilcox <drakewilcox@gmail.com>
2020-03-25 10:44:44 -0700

    add appropriate 'virtual' syntax for reference.
    Co-authored-by: Jamison Cozart <jamisoncozart@gmail.com>
    Co-authored-by: Geoff Goetz <geoff.goetz503@gmail.com>

--- RecipeBox/Models/CategoryRecipe.cs
+++ RecipeBox/Models/CategoryRecipe.cs
@@ -5,7 +5,7 @@ namespace RecipeBox.Models
     public int CategoryRecipeId { get; set; }
     public int RecipeId { get; set; }
     public int CategoryId { get; set; }
-    public Recipe Recipe { get; set; }
-    public Category Category { get; set; }
+    public virtual Recipe Recipe { get; set; }
+    public virtual Category Category { get; set; }
   }
 }
--- RecipeBox/Models/Recipe.cs
+++ RecipeBox/Models/Recipe.cs
@@ -12,6 +12,6 @@ namespace RecipeBox.Models
 		public string Name { get; set; }
 		public string Description { get; set; }
 		public virtual ApplicationUser User { get; set; }
-		public ICollection<CategoryRecipe> Categories { get; }
+		public virtual ICollection<CategoryRecipe> Categories { get; }
 	}
 }
